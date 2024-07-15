5f313e2d79bdf5964ab337ee3f4f5250bc8d3618
SodiqSWE <sodiq260@gmail.com>
2024-07-15 14:51:55 -0400

    Changed background color and header color

--- my-portfolio/src/components/About.jsx
+++ my-portfolio/src/components/About.jsx
@@ -5,10 +5,11 @@ import React from 'react';
 const About = () => {
   return (
     <section id="about">
-      <h2>Hey, my name is Sodiq!</h2>
       <div className='flex-container'>
         <img src={headshot} alt='Headshot' className='image-left'/>
-        <p>I'm a software engineer with experience in both Front-End and Back-End scopes. I love designing and developing web and mobile applications, games, and learning new technologies!
+        <p>
+          <h2>Hey, my name is <span className='my-name'>Sodiq!</span></h2>
+          I'm a software engineer with experience in both Front-End and Back-End scopes. I love designing and developing web and mobile applications, games, and learning new technologies!
           Other than coding, I love PC building, Soccer, I'm a huge anime nerd and car nerd!
         </p>
       </div>
--- my-portfolio/src/components/Header.jsx
+++ my-portfolio/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Header = () => {
   return (
-    <header>
+    <header className='header'>
       <nav>
         <a href="#about">About</a>
         <a href="#projects">Projects</a>
