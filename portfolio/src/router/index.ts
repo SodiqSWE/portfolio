3001eb29df06fe4dbace1317bb2d2e2f6cd3e4c2
SodiqSWE <sodiq260@gmail.com>
2024-12-04 23:17:37 -0500

    added some placeholder components

---
+++ portfolio/src/router/index.ts
@@ -0,0 +1,17 @@
+import { createRouter, createWebHistory } from "vue-router";
+import About from '../components/About.vue';
+import Projects from '../components/Projects.vue';
+import Contact from '../components/Contact.vue';
+
+const routes = [
+    { path: '/', component: About },
+    { path: '/projects', component: Projects },
+    { path: '/contact', component: Contact },
+  ];
+  
+  const router = createRouter({
+    history: createWebHistory(),
+    routes,
+  });
+  
+  export default router;
