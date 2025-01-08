fe44b92a6847c50fa8af75aa3029198ebd3f4892
SodiqSWE <sodiq260@gmail.com>
2025-01-08 13:50:24 -0500

    working on debugging the api url in the netlify function

--- netlify/functions/steamProxy.js
+++ netlify/functions/steamProxy.js
@@ -1,28 +1,29 @@
 import fetch from 'node-fetch';
 
 exports.handler = async (event) => {
-  const { path } = event;
-  const apiUrl = `https://api.steampowered.com${path.replace('/.netlify/functions/steamProxy', '')}`;
+    console.log('Event Path:', event.path);
+    const { path } = event;
+    const apiUrl = `https://api.steampowered.com${path.replace('/.netlify/functions/steamProxy', '')}`;
 
-  console.log('API URL: ', apiUrl)
+    console.log('API URL: ', apiUrl)
 
-  try {
-    const response = await fetch(apiUrl, {
-      headers: {
-        'Authorization': `Bearer ${process.env.STEAM_API_KEY}`
-      },
-    });
-    const data = await response.json();
+    try {
+        const response = await fetch(apiUrl, {
+            headers: {
+                'Authorization': `Bearer ${process.env.STEAM_API_KEY}`
+            },
+        });
+        const data = await response.json();
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(data),
-    };
-  } catch (error) {
-    console.error('Error:', error);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: 'Failed to fetch data', details: error.message }),
-    };
-  }
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data),
+        };
+    } catch (error) {
+        console.error('Error:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Failed to fetch data', details: error.message }),
+        };
+    }
 };
